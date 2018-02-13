29adabb552ece9622ca67002597f74da4df76a80
Jarrett Helton <j.helton@kunzleigh.com>
2018-02-13 09:59:32 -0500

    adding starting template for static site

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    console.log("Hey Im jQuery code and im ready to mingle");
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
